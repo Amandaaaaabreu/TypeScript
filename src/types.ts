9617764b56e0b1151b0f88a6045fa7b4ccb76e84
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-05-05 14:32:58 -0300

    att ts

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+// boalean (true/false) 
+let isOpen : boolean
+isOpen = true
+
+// string ('amanda') ("amanda") (`amanda`)
+let message : string
+message = `amanda ${isOpen}` // concatenação
+
+//number (int, float, hexadecimal, binario
+let total : number
+total = 20 
+
+//array (type[])
+let item : number[]
+item = [1,2,3,42]
+
+let p : string[]
+p : ['amanda', 'giselle', 'giovanna']
+
+//array (type[])
+let value : Array<number>
+value = [1,5,9]
