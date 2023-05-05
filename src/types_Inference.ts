a9b158ed3a6851af9a41fcc6e4e74f0e622c8de0
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-05-05 15:45:48 -0300

    att type inference

---
+++ src/types_Inference.ts
@@ -0,0 +1,11 @@
+// type inference
+
+let mensagem = "oi amanda"  // nao preciso declarar o tipo
+
+// Evitar de ficar tipando desnecessáriamente
+
+window.addEventListener("click",(e) => {   // eu nao falei de que tipo é esse (e)
+    console.log(e.target);
+    
+
+})
