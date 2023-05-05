7a4c84b71d988b814ea653f03febb73bb50a2673
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-05-05 14:44:38 -0300

    att

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,7 @@ isOpen = true
 let message : string
 message = `amanda ${isOpen}` // concatenação
 
-//number (int, float, hexadecimal, binario
+//number (int, float, hexadecimal, binario)
 let total : number
 total = 20 
 
@@ -20,3 +20,26 @@ p : ['amanda', 'giselle', 'giovanna']
 //array (type[])
 let value : Array<number>
 value = [1,5,9]
+
+// tuple é um array (na tupla sabemos a quantidade de elemento dentro dele e o seu tipo)
+let title : [number, string, boolean]
+title = [1, 'a', true]
+
+// enum (serve para criar um conjunto de chaveValor)
+enum colors{ 
+    white = '#fff',
+    black = '#000'
+}
+
+// any (pode ser QUALQUER COISA) (exclusivo do typeScript) 
+let coisa : any 
+coisa = 'amanda' // string
+
+let coisa1 : any 
+coisa1 = true   // bolean
+
+let coisa2 : any 
+coisa2 = 2  //number
+
+let coisa3 : any 
+coisa2 = ['amanda', 2] // array
