2c9e80f3da4c2c7efb6b7ac33cab343750cc5324
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-05-08 17:55:27 -0300

    att classes ts

--- src/classes.js
+++ src/classes.js
@@ -1,14 +1,23 @@
 "use strict";
 // as classes não sao obrigatórias no typescript.
 class userConta {
-    constructor(name, id, idade) {
+    constructor(name, idade) {
         this.name = name;
-        this.id = id;
         this.idade = idade;
     }
     logDetails() {
         console.log(`The players ${this.name} is ${this.idade} years old.`);
     }
 }
-const will = new userConta('Amanda', 7, 20);
+class contaEscolhida extends userConta {
+    constructor(name, idade, sexo, nickname) {
+        super(name, idade);
+        this.sexo = sexo;
+        this.nickname = nickname;
+    }
+}
+const will = new userConta('Amanda', 20);
 console.log(will);
+will.logDetails();
+const john = new contaEscolhida("John", 20, 'masculino', 'jhonmaster');
+console.log(john);
--- src/classes.ts
+++ src/classes.ts
@@ -2,19 +2,33 @@
 
 class userConta{
     name: string;
-    id: number;
     idade: number;
 
-    constructor(name:string, id:number, idade:number){
+    constructor(name:string, idade:number){
         this.name = name;
-        this.id = id;
         this.idade = idade;
     }
 
     logDetails(): void {
         console.log(`The players ${this.name} is ${this.idade} years old.`);
     }
+    
 }
 
-const will = new userConta('Amanda', 7,20);
+class contaEscolhida extends userConta{
+    sexo: string;
+    nickname: string;
+
+    constructor(name:string, idade:number, sexo:string, nickname:string){
+        super(name, idade);
+        this.sexo = sexo;
+        this.nickname = nickname;
+    }
+}
+
+const will = new userConta('Amanda' , 20);
 console.log(will);
+will.logDetails();
+
+const john = new contaEscolhida("John", 20, 'masculino', 'jhonmaster');
+console.log(john);
