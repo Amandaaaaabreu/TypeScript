5e7fb8745c19ea3851052a3948705eb6fb5b4f65
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-05-08 21:10:45 -0300

    att interface ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,7 +3,7 @@ interface Tela{
     description: string;
     genre: string;
     plataform: string[];
-    getSmilars: (title: string) => void;
+    getSmilars?: (title: string) => void;
 }
 
 
@@ -18,4 +18,23 @@ const tlou : Tela = {
 }
 
 console.log(tlou.title);
-tlou.getSmilars(tlou.title);
+
+if(tlou.getSmilars){ // se existir meu metodo getSimilars aí eu chamo ele
+    tlou.getSmilars(tlou.title);
+}
+
+
+interface DLC extends Tela{ // extendo a interface
+    originalTela: Tela;  // informacoes da interface original.
+    newContent: string[];
+}
+
+const leftbehind: DLC = { // passando as informacoes da interface original para a DLC.
+       title: "The Last of Us - Left Behind",
+       description:"You play as Ellie before the original game",
+       genre: "Action",
+       plataform: ['PS5'],
+       originalTela: tlou,
+       newContent: ["3 hours story","new characteres"],
+
+};
